eec92b6792f71e83c354633a38071b3d27272f08
LAXMI NARAYAN SAHOO <140277278+PUPUN-SAHOO@users.noreply.github.com>
2026-01-04 19:58:26 +0530

    Update portfolio

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
+(function () {
+  emailjs.init("IyMurxSTkpSP_qmeO"); // ✅ Your Public Key
+})();
+
 const form = document.getElementById("contactForm");
 const success = document.getElementById("success");
 
-form.addEventListener("submit", e => {
+form.addEventListener("submit", function (e) {
   e.preventDefault();
-  success.textContent = "Message sent successfully!";
-  form.reset();
+
+  emailjs.send(
+    "service_4x9up6f",     // ✅ Your Service ID
+    "template_u29nqyw",    // ✅ Your Template ID
+    {
+      name: form.name.value,
+      email: form.email.value,
+      message: form.message.value,
+    }
+  )
+  .then(() => {
+    success.innerText = "Message sent successfully ✅";
+    success.style.color = "#22d3ee";
+    form.reset();
+  })
+  .catch((error) => {
+    success.innerText = "Message failed ❌";
+    success.style.color = "red";
+    console.error("EmailJS Error:", error);
+  });
 });
